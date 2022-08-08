693bba975fbde0f6db22b8b1aea2f10b6ca47d0b
Hamza Jamil <lyallpurlad@gmail.com>
2022-08-08 15:18:21 +0500

    Set windows configs

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service#...
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import pandas as pd
 import time
 import os
+from os.path import join, dirname #..
 from dotenv import load_dotenv
 from threading import Thread
+
+#..
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 webURL = "https://account.microsoft.com/billing/orders"
 emailID = 'i0116'
@@ -20,18 +26,20 @@ class TimerScraper(Thread):
             print(x+1, '...', end='')
             time.sleep(1)
 
+#...
+s = Service("C:\Program Files (x86)\chromedriver.exe")
+driver = webdriver.Chrome(service=s)
 
-driver = webdriver.Chrome()
 driver.get(webURL)
 
 time.sleep(timeToWait+1)
 email = driver.find_element(By.ID, emailID)
-email.send_keys(os.getenv('EMAIL'))
+email.send_keys(os.environ.get('EMAIL')) # ..
 email.send_keys(Keys.RETURN)
 
 time.sleep(timeToWait)
 password = driver.find_element(By.ID, passID)
-password.send_keys(os.getenv('PASSWORD'))
+password.send_keys(os.environ.get('PASSWORD')) # ..
 password.send_keys(Keys.RETURN)
 
 time.sleep(timeToWait)
@@ -51,19 +59,15 @@ print('\n\nTotal Order Scraped: ', count)
 
 for x in range(1, count+1):
     try:
-        giftcard = driver.find_element(By.XPATH, "//*[@id='order-history-wrapper']/div/div[3]/div["+str(
-            x)+"]/div/div/div[2]/div/div[3]/div/div/div//button[@aria-label='View gift code']")
+        giftcard = driver.find_element(By.XPATH, "//*[@id='order-history-wrapper']/div/div[3]/div["+str(x)+"]/div/div/div[2]/div/div[3]/div/div/div//button[@aria-label='View gift code']")
         driver.execute_script("arguments[0].click();", giftcard)
         alert = driver.switch_to.active_element
         alert.click()
         copied_data = pd.read_clipboard()
         giftCardNumber = copied_data.columns[0]
-        dateShate = driver.find_element(
-            By.XPATH, "//*[@id='order-history-wrapper']/div/div[3]/div["+str(x)+"]//div/div/div[1]/span/span[1]")
-        orderShorder = driver.find_element(
-            By.XPATH, "//*[@id='order-history-wrapper']/div/div[3]/div["+str(x)+"]//div/div/div[1]/span/span[3]")
-        print('Gift Card Number:', giftCardNumber, ' Date: ',
-              dateShate.text, ' Order#: ', orderShorder.text)
+        dateShate = driver.find_element(By.XPATH, "//*[@id='order-history-wrapper']/div/div[3]/div["+str(x)+"]//div/div/div[1]/span/span[1]")
+        orderShorder = driver.find_element(By.XPATH, "//*[@id='order-history-wrapper']/div/div[3]/div["+str(x)+"]//div/div/div[1]/span/span[3]")
+        print('Gift Card Number:', giftCardNumber, ' Date: ',dateShate.text, ' Order#: ', orderShorder.text)
     except:
         continue
 
